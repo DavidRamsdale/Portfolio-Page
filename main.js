d106cb718cbcce050eff6ef7f9964043859a9034
David Ramsdale <davidramsdale1991@gmail.com>
2018-12-04 17:55:10 +1100

    Added styling to navbar

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Menu Toggle
 function toogleNavListOpen () {
     let navlist = document.getElementById("nav-list");
     navlist.classList.toggle("open");
@@ -7,3 +8,14 @@ document.getElementById("hamburger").addEventListener("click", toogleNavListOpen
 
 let navlistitems = document.querySelectorAll("#nav-list li");
 navlistitems.forEach(item => item.addEventListener("click", toogleNavListOpen));
+
+//Scrolling effect
+
+$window.on("scroll", function(){
+    if($window).scrollTop()) {
+        $("nav").addClass('black');
+    } 
+    else{
+        $("nav").removeClass('black');
+    }
+})
